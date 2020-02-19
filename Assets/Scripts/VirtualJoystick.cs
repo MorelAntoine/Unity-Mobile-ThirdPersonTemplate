2226fa6ce12b6912bd777953b5fd3c810642555b
MorelAntoine <antoinemorel@outlook.com>
2020-02-19 18:20:11 +0100

    Add controller reset, controller range refactor OnDrag

--- Assets/Scripts/VirtualJoystick.cs
+++ Assets/Scripts/VirtualJoystick.cs
@@ -5,62 +5,55 @@ using UnityEngine.EventSystems;
 namespace ThirdPersonTemplate
 {
     /// <summary>
-    /// Class used to simulate a joystick. 
+    /// Class used to virtually simulate a joystick.
     /// </summary>
+    [DisallowMultipleComponent]
     public sealed class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
     {
-        ///////////////////////////////
-        ////////// Attribute //////////
-        ///////////////////////////////
+        ////////////////////////////////
+        ////////// Attributes //////////
+        ////////////////////////////////
 
+        ////////////////////////////////
+        ////////// Components //////////
+
+        private Image _backgroundImage = null;
+        private Image _controllerImage = null;
+
+        ///////////////////////////////////////////
+        ////////// Generated Information //////////
+
+        [Header("Generated Information")]
         [SerializeField] private Vector2 _virtualInput = Vector2.zero;
 
-        /////////////////////////////////
-        ////////// UI Resource //////////
-
-        private Image _joystickAreaImage = null;
-        private Image _joystickControllerImage = null;
-
         //////////////////////////////
-        ////////// Property //////////
-        //////////////////////////////
+        ////////// Settings //////////
 
+        [Header("Settings")]
+        [SerializeField, Range(2f, 4f)] private float _controllerRange = 2.7f;
+
+        ////////////////////////////////
+        ////////// Properties //////////
+        ////////////////////////////////
+
+        public float HorizontalInput => _virtualInput.x;
+        public float VerticalInput => _virtualInput.y;
         public Vector2 VirtualInput => _virtualInput;
 
-        public float Horizontal => _virtualInput.x;
+        /////////////////////////////
+        ////////// Methods //////////
+        /////////////////////////////
 
-        public float Vertical => _virtualInput.y;
-
-        ////////////////////////////
-        ////////// Method //////////
-        ////////////////////////////
-
-        //////////////////////////////////////////////
-        ////////// Interface Implementation //////////
+        ///////////////////////////////////////////////
+        ////////// Interfaces Implementation //////////
 
         public void OnDrag(PointerEventData eventData)
         {
-            Vector2 outLocalPoint;
-            Vector2 inputVector;
-            Vector2 inputVectorClamped;
-            Vector2 joystickControllerPosition;
-
-            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_joystickAreaImage.rectTransform,
-                eventData.position, eventData.pressEventCamera, out outLocalPoint))
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_backgroundImage.rectTransform,
+                eventData.position, eventData.pressEventCamera, out Vector2 outLocalPoint))
             {
-                inputVector.x = outLocalPoint.x / _joystickAreaImage.rectTransform.sizeDelta.x;
-                inputVector.y = outLocalPoint.y / _joystickAreaImage.rectTransform.sizeDelta.y;
-
-                inputVectorClamped.x = inputVector.x * 2 - 1;
-                inputVectorClamped.y = inputVector.y * 2 - 1;
-
-                _virtualInput = inputVectorClamped.magnitude > 1.0f ? inputVectorClamped.normalized : inputVectorClamped;
-
-                Debug.Log(_virtualInput);
-
-                joystickControllerPosition.x = _virtualInput.x * (_joystickAreaImage.rectTransform.sizeDelta.x / 2.5f);
-                joystickControllerPosition.y = _virtualInput.y * (_joystickAreaImage.rectTransform.sizeDelta.y / 2.5f);
-                _joystickControllerImage.rectTransform.anchoredPosition = joystickControllerPosition;
+                CalculateVirtualInput(in outLocalPoint);
+                UpdateControllerPosition();
             }
         }
 
@@ -72,16 +65,41 @@ namespace ThirdPersonTemplate
         public void OnPointerUp(PointerEventData eventData)
         {
             _virtualInput = Vector2.zero;
-
+            _controllerImage.rectTransform.anchoredPosition = Vector2.zero;
         }
 
-        //////////////////////////////////////////////
-        ////////// MonoBehaviour Callback ////////////
+        /////////////////////////////////////////////
+        ////////// MonoBehaviour Callbacks //////////
 
         private void Awake()
         {
-            _joystickAreaImage = GetComponent<Image>();
-            _joystickControllerImage = GetComponentInChildren<Image>();
+            _backgroundImage = GetComponent<Image>();
+            _controllerImage = transform.GetChild(0).GetComponent<Image>();
         }
+
+        //////////////////////////////
+        ////////// Services //////////
+
+        void CalculateVirtualInput(in Vector2 localPoint)
+        {
+            Vector2 inputVector;
+            Vector2 inputVectorClamped;
+
+            inputVector.x = localPoint.x / _backgroundImage.rectTransform.sizeDelta.x;
+            inputVector.y = localPoint.y / _backgroundImage.rectTransform.sizeDelta.y;
+            inputVectorClamped.x = inputVector.x * 2 - 1;
+            inputVectorClamped.y = inputVector.y * 2 - 1;
+            _virtualInput = inputVectorClamped.magnitude > 1.0f ? inputVectorClamped.normalized : inputVectorClamped;
+        }
+
+        void UpdateControllerPosition()
+        {
+            Vector2 controllerPosition;
+
+            controllerPosition.x = _virtualInput.x * (_backgroundImage.rectTransform.sizeDelta.x / _controllerRange);
+            controllerPosition.y = _virtualInput.y * (_backgroundImage.rectTransform.sizeDelta.y / _controllerRange);
+            _controllerImage.rectTransform.anchoredPosition = controllerPosition;
+        }
+
     }
 }
