a92aaa5b3a4aff96115b1068ebdac66a5096de0a
MorelAntoine <antoinemorel@outlook.com>
2020-02-20 17:12:02 +0100

    Add invert axis settings (vertical & horizontal)

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -23,6 +23,8 @@ namespace ThirdPersonTemplate
 
         [Header("Settings")]
         [SerializeField, Range(2f, 9f)] private float _distanceFromSubject = 5.5f;
+        [SerializeField] private bool _invertVerticalAxis = true;
+        [SerializeField] private bool _invertHorizontalAxis = true;
         [SerializeField, Range(20f, 160f)] private float _rotationSpeed = 80f;
         [SerializeField] private Vector3 _subjectOffset = Vector3.zero;
         [SerializeField, Range(0f, 70f)] private float _verticalLimit = 35f;
@@ -50,8 +52,10 @@ namespace ThirdPersonTemplate
 
         private void UpdateRotation()
         {
-            float deltaHorizontalRotation = _inputSource.HorizontalInput * _rotationSpeed * Time.deltaTime;
-            float deltaVerticalRotation = _inputSource.VerticalInput * _rotationSpeed * Time.deltaTime;
+            float horizontalInput = _invertHorizontalAxis ? -_inputSource.HorizontalInput : _inputSource.HorizontalInput;
+            float verticalInput = _invertVerticalAxis ? -_inputSource.VerticalInput : _inputSource.VerticalInput;
+            float deltaHorizontalRotation = horizontalInput * _rotationSpeed * Time.deltaTime;
+            float deltaVerticalRotation = verticalInput * _rotationSpeed * Time.deltaTime;
             float clampedXLocalRotation = Mathf.Clamp(transform.localEulerAngles.x + deltaVerticalRotation, 0, _verticalLimit);
 
             transform.localEulerAngles = new Vector3(clampedXLocalRotation,
